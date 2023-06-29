f2114b8ffaace90b46853fe5d9282d2a198c871b
Ng Kok Sheng <118619477+Ng-Kok-Sheng@users.noreply.github.com>
2023-06-29 18:52:32 +0800

    initialize constants

---
+++ front-end/src/constants/index.ts
@@ -0,0 +1,45 @@
+type feedbackConstant = {
+  title: string;
+  value: string;
+  emoji: string;
+  feedbackList: string[];
+};
+
+export const feedbackMood: feedbackConstant[] = [
+  {
+    title: "Awesome!",
+    value: "awesome",
+    emoji: "😍",
+    feedbackList: [
+      "I did not feel bored at all.",
+      "I understood almost everything.",
+      "I can do all the activities.",
+      "I enjoyed interacting with my tutor.",
+      "I enjoyed interacting with my classmates.",
+    ],
+  },
+  {
+    title: "Okay.",
+    value: "okay",
+    emoji: "😐",
+    feedbackList: [
+      "I felt bored a few times.",
+      "I can only understand some parts.",
+      "I can only do some of the activities.",
+      "I wish to interact more with my tutor.",
+      "I wish to interact more with my classmates.",
+    ],
+  },
+  {
+    title: "Terrible...",
+    value: "terrible",
+    emoji: "😭",
+    feedbackList: [
+      "I felt bored most of the time.",
+      "I can hardly understand anything.",
+      "I can hardly do any of the activities.",
+      "I wish to interact more with my tutor.",
+      "I wish to interact more with my classmates.",
+    ],
+  },
+];
