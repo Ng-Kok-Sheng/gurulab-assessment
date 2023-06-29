a108ad5ad3a61aeac2c091265ab1f33e0a23e418
Ng Kok Sheng <118619477+Ng-Kok-Sheng@users.noreply.github.com>
2023-06-29 20:13:34 +0800

    3 second delay to open feedback

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+import { PopupModal, FeedbackForm } from "./components";
+
 function App() {
-  return <div></div>;
+  const [showFeedbackModal, setShowFeedbackModal] = useState(false);
+
+  useEffect(() => {
+    const threeSecondDelay = () => {
+      return new Promise(() => {
+        setTimeout(() => {
+          setShowFeedbackModal(true);
+        }, 3000);
+      });
+    };
+
+    threeSecondDelay();
+  }, []);
+
+  return (
+    <div>
+      <PopupModal showModal={showFeedbackModal} modalWidth="w-2/4">
+        <FeedbackForm closeModal={() => setShowFeedbackModal(false)} />
+      </PopupModal>
+    </div>
+  );
 }
 
 export default App;
