a765fec85f092e042bbb6382c22a62a53677886e
Ng Kok Sheng <118619477+Ng-Kok-Sheng@users.noreply.github.com>
2023-06-29 20:13:15 +0800

    Update config and constants

--- front-end/src/constants/index.ts
+++ front-end/src/constants/index.ts
@@ -2,7 +2,10 @@ type feedbackConstant = {
   title: string;
   value: string;
   emoji: string;
-  feedbackList: string[];
+};
+
+type feedbackListConstant = {
+  [key: string]: string[];
 };
 
 export const feedbackForm = {
@@ -12,39 +15,42 @@ export const feedbackForm = {
 
 export const feedbackMood: feedbackConstant[] = [
   {
-    title: "Awesome!",
-    value: "awesome",
-    emoji: "😍",
-    feedbackList: [
-      "I did not feel bored at all.",
-      "I understood almost everything.",
-      "I can do all the activities.",
-      "I enjoyed interacting with my tutor.",
-      "I enjoyed interacting with my classmates.",
-    ],
+    title: "Terrible...",
+    value: "terrible",
+    emoji: "😭",
   },
   {
     title: "Okay.",
     value: "okay",
     emoji: "😐",
-    feedbackList: [
-      "I felt bored a few times.",
-      "I can only understand some parts.",
-      "I can only do some of the activities.",
-      "I wish to interact more with my tutor.",
-      "I wish to interact more with my classmates.",
-    ],
   },
   {
-    title: "Terrible...",
-    value: "terrible",
-    emoji: "😭",
-    feedbackList: [
-      "I felt bored most of the time.",
-      "I can hardly understand anything.",
-      "I can hardly do any of the activities.",
-      "I wish to interact more with my tutor.",
-      "I wish to interact more with my classmates.",
-    ],
+    title: "Awesome!",
+    value: "awesome",
+    emoji: "😍",
   },
 ];
+
+export const feedbackList: feedbackListConstant = {
+  awesome: [
+    "I did not feel bored at all.",
+    "I understood almost everything.",
+    "I can do all the activities.",
+    "I enjoyed interacting with my tutor.",
+    "I enjoyed interacting with my classmates.",
+  ],
+  okay: [
+    "I felt bored a few times.",
+    "I can only understand some parts.",
+    "I can only do some of the activities.",
+    "I wish to interact more with my tutor.",
+    "I wish to interact more with my classmates.",
+  ],
+  terrible: [
+    "I felt bored most of the time.",
+    "I can hardly understand anything.",
+    "I can hardly do any of the activities.",
+    "I wish to interact more with my tutor.",
+    "I wish to interact more with my classmates.",
+  ],
+};
